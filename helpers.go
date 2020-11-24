6ec2fdd88dd4cfe34a57410c1812cc3a7270f0cf
Anthony <anthonyavardaro67@gmail.com>
2020-11-24 02:53:37 -0600

    Add some clarification

--- helpers.go
+++ helpers.go
@@ -3,7 +3,14 @@ package gopolygon
 /*
 	This file contains convenience functions to extract the address from a literal.
 
-	This is supposed to make it easier to use the query-as-a-struct design I've chosen
+	This is supposed to make it easier to use the query-as-a-struct design I've chosen.
+	In Go it's pretty much impossible to do something like:
+		a := &5
+	Instead, you'd need to do something to the effect of
+		other := 5
+		a := %other
+	Or do so with a function, which is what the file aims to shorten (it can get pretty verbose)
+	https://willnorris.com/2014/05/go-rest-apis-and-pointers/
 */
 
 // String just returns the address of the string
