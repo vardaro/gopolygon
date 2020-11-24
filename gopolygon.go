17b2fe756be07bd0ae1ec4cda2e4f435421a6688
Anthony <anthonyavardaro67@gmail.com>
2020-11-23 22:00:17 -0600

    Restructure file

---
+++ gopolygon.go
@@ -0,0 +1,11 @@
+package gopolygon
+
+// Client is polygon api client
+type Client struct {
+	APIKey string
+}
+
+// NewClient returns a new Client instance.
+func NewClient(apikey string) *Client {
+	return &Client{APIKey: apikey}
+}
